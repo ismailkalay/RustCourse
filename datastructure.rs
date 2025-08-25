3de0c60a1260017a969a4bb1f43fda476e131c42
ismail kalay <ismailkalay@hotmail.com>
2025-08-25 15:15:34 +0300

    Add files via upload

--- datastructure.rs
+++ datastructure.rs
@@ -27,14 +27,72 @@ fn arrays(){
 
 }
 fn matris(){
+    let mtx:[[f32;3];2] =[
+        [1.0,0.0,0.0], [0.0,1.0,0.0]];
 
+    println!("{:?} ",mtx);
+
+    for i in 0..mtx.len(){
+        for j in 0..mtx[i].len(){
+            println!("mtx[{}][{}]= {}",i,j,mtx[i][j]);
+        }
+
+    }
+
+}
+
+fn use_slice(slice:&mut[i32]){
+    println!("new data slice first member {}, member number {}.",slice[0],slice.len());
+    slice[0]=3333;
+}
+fn slices(){
+    let mut data:[i32;5] = [1,2,3,4,5];
+    use_slice(&mut data[1..4]);
+    println!("{:?}",data);
+}
+
+fn topla_ve_carp(x:i32,y:i32)-> (i32,i32){
+    (x+y,x*y)
+
+}
+
+fn tuples(){
+    let x =3;
+    let y =4;
+    let sonuclar = topla_ve_carp(x,y);
+    println!("results : {:?}", sonuclar);
+    println!("{0} + {1} = {2} and {0}*{1}={3}",x,y,sonuclar.0,sonuclar.1);
+
+    let elemanlar = (true, 24.1, -1i8);
+    println!("members :{:?}",elemanlar);
+
+
+
+}
+
+struct  Point <T,V> {
+    x:T,
+    y:V
+}
+
+struct Line <T,V,S,K>{
+    start:Point<T,V>,
+    end:Point<S,K>
 }
 
 fn main(){
     
     //arrays();
-    matris();
-    
+    //matris();
+    //slices();
+    //tuples();
+
+    let a = Point{x:4.5,y:7.5};
+    let b = Point{x:4,y:7};
+    let cizgi = Line{start:Point{x:2,y:2.4}, end:Point{x:4,y:2.3}};
+
+    println!("a = ({},{})",a.x,a.y);
+    println!("b = ({},{})",b.x,b.y);
 
 
 }
